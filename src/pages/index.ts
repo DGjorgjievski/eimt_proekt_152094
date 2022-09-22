47abe2742b5b156eb35015b63cc2a9e7c7d9e85e
Davor Gjorgjievski <gjorgjievskidavor@gmail.com>
2022-09-22 23:59:06 +0200

    clean up

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import { Register } from './Register/Register';
-export { Home } from "./Home/Home";
-export { User } from "./User/User";
-export { Login } from "./Login/Login";
-export { Register } from "./Register/Register";
