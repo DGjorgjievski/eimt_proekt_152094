8ec2d83acf0dd8063486f6fb41cb5d9ff3f39785
Davor Gjorgjievski <gjorgjievskidavor@gmail.com>
2022-09-22 02:16:38 +0200

    CORS settings

--- src/main/java/com/example/LocalReservationApp/controller/TableController.java
+++ src/main/java/com/example/LocalReservationApp/controller/TableController.java
@@ -4,6 +4,7 @@ import com.example.LocalReservationApp.service.TableService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 @AllArgsConstructor
 @Validated
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TableController {
 
     private final TableService tableService;
